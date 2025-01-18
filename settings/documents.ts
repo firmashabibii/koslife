d60ba8973b9b69e421a48ce20c53ade3b120bbd1
firmashabibii <firmashabibi71@gmail.com>
2025-01-18 21:53:46 +0700

    bisalah

--- settings/documents.ts
+++ settings/documents.ts
@@ -33,27 +33,23 @@ export const Documents: Paths[] = [
     href: "/pendahuluan",
     items: [
       {
-        title: "Deskripsi Proyek",
+        title: "Deskripsi Projek",
         href: "/deskripsi_projek",
       },
       {
-        title: "Teknologi Yang digunakan",
-        href: "/teknologi_yang_digunakan",
+        title: "Teknologi yang Digunakan",
+        href: "/teknologiyang_digunakan",
       },
-      {
-        title: "Keterkaitan Matkul",
-        href: "/keterkaitan_matkul",
-      },
-    ]
+    ],
   },
   {
-    title: "Analisis Desain Perangkat Lunak",
-    href: "/structure",
+    title: "Analisis dan Desain Perangkat Lunak",
+    href: "/analisis_desain",
     items: [
       {
         title: "Analisis Kebutuhan",
         href: "/analisis_kebutuhan",
-        items : [
+        items: [
           {
             title: "Business Requirement",
             href: "/business_requirement",
@@ -70,12 +66,12 @@ export const Documents: Paths[] = [
             title: "Software Requirement",
             href: "/software_requirement",
           },
-        ]
+        ],
       },
       {
         title: "Diagram",
         href: "/diagram",
-        items : [
+        items: [
           {
             title: "Use Case Diagram",
             href: "/use_case_diagram",
@@ -88,57 +84,147 @@ export const Documents: Paths[] = [
             title: "Flowchart",
             href: "/flowchart",
           },
-        ]
+        ],
       },
     ],
   },
   {
-    title: "Perancangan Antarmuka",
-    href: "/perancangan_antarmuka",
+    title: "Perancangan dan Antarmuka",
+    href: "/perancanganantarmuka",
     items: [
       {
         title: "Dokumen Desain",
-        href: "/dokumen_desain",
-        items : [
+        href: "/deeper",
+        items: [
           {
             title: "Sketsa Awal",
-            href: "/sketsa_awal",
+            href: "/sketsa",
+          },
+          {
+            title: "Wireframe",
+            href: "/wireframe",
+          },
+          {
+            title: "Userflow",
+            href: "/userflow",
           },
           {
             title: "Prototipe",
-            href: "/prototipe",
+            href: "/prototaipe",
           },
-        ]
+        ],
+      },
+      {
+        title: "Implementasi Desain",
+        href: "/implementasi",
+        items: [
+          {
+            title: "Kode Html, CSS, JavaScript",
+            href: "/even-deeper",
+          },
+        ],
       },
     ],
   },
   {
-    title: "Metode Pengembangan Perangkat Lunak",
-    href: "/metode_pengembangan_perangkat_lunak",
+    title: "Pengembangan Aplikasi",
+    href: "/pengembanganaplikasi",
     items: [
       {
+        title: "Fitur Utama",
+        href: "/fitur",
+        items: [
+          {
+            title: "Operasi CRUD",
+            href: "/operasicrud",
+          },
+          {
+            title: "Fitur Login dan Registrasi",
+            href: "/fiturlogin",
+          },
+          {
+            title: "Fitur Interaktif",
+            href: "/interaktif",
+          },
+        ],
+      },
+      {
+        title: "Struktur Basis Data",
+        href: "/struktur",
+        items: [
+          {
+            title: "MYSQL",
+            href: "/mysql",
+          },
+        ],
+      },
+      {
         title: "Metode SDLC",
-        href: "/metode_sdlc",
-        items : [
+        href: "/metodesdlc",
+        items: [
           {
             title: "Analisis Kebutuhan",
-            href: "/analisis_kebutuhan",
+            href: "/kebutuhan",
           },
           {
-            title: "Diagram UML",
-            href: "/diagram_uml",
+            title: "Pembuatan Diagram UML",
+            href: "/diagramuml",
           },
-        ]
+          {
+            title: "Pengembangan Kode dan Basis Data",
+            href: "/basisdata",
+          },
+          {
+            title: "Pengujian Fungsionalitas",
+            href: "/penguji",
+          },
+          {
+            title: "Deployment ke Vercel",
+            href: "/deployment",
+          },
+        ],
       },
     ],
   },
   {
     title: "Dokumentasi Penggunaan Website",
-    href: "/dokumentasi_penggunaan_website",
+    href: "/dokumentasipenggunaan",
     items: [
       {
         title: "Panduan Penggunaan Website",
-        href: "/panduan_penggunaan_website",
+        href: "/deeper",
+      },
+    ],
+  },
+  {
+    title: "Metode Pengembangan Perangkat Lunak",
+    href: "/metodepengembangan",
+    items: [
+      {
+        title: "Penjelasan Metode yang Digunakan",
+        href: "/penjelasanmetode",
+      },
+      {
+        title: "Alasan Memilih Metode",
+        href: "/alasanmetode",
+      },
+      {
+        title: "Implementasi Setiap Tahapan",
+        href: "/tahap",
+      },
+    ],
+  },
+  {
+    title: "Kesimpulan dan Saran",
+    href: "/kesimpulan",
+    items: [
+      {
+        title: "Evaluasi Projek",
+        href: "/evaluasi",
+      },
+      {
+        title: "Rekomendai Pengembangan Selanjutnya",
+        href: "/rekomendasi",
       },
     ],
   },
